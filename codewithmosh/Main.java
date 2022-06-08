a59fafd6d69235f27b5ef30c0ce1d2701854675d
Irfa Sheikh <78042313+irfasheikh@users.noreply.github.com>
2022-06-08 14:10:23 -0500

    purractice

---
+++ codewithmosh/Main.java
@@ -0,0 +1,26 @@
+package codewithmosh;
+import java.awt.*;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args){
+//        byte age = 30;
+//        Date now = new Date();
+//        System.out.println(now);
+
+//        Point point1 = new Point(1,1);
+//        Point point2 = point1; // referring to the object in the memory
+//        //point 1 and 2 are referencing the same object so if we change one, both will change
+//        point1.x = 2;
+//        System.out.println(point2);
+
+        String message = "Hello World" + "!!";
+        message.endsWith("!!");
+        System.out.println((message.startsWith("!!")));
+
+
+
+        //use underscore to separate 3 digits like a comma
+
+    }
+}
